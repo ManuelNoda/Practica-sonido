53c829d4f340340a00814d6860d84efa4a6f3961
Manuel <91590213+ManuelNoda@users.noreply.github.com>
2025-12-09 12:53:26 +0000

    Add files via upload

---
+++ MicrophoneDemo.cs
@@ -0,0 +1,148 @@
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Whisper.Utils;
+using Button = UnityEngine.UI.Button;
+using Toggle = UnityEngine.UI.Toggle;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace Whisper.Samples
+{
+    /// <summary>
+    /// Record audio clip from microphone and make a transcription.
+    /// </summary>
+    public class MicrophoneDemo : MonoBehaviour
+    {
+        public WhisperManager whisper;
+        public MicrophoneRecord microphoneRecord;
+        public bool streamSegments = true;
+        public bool printLanguage = true;
+
+        [Header("UI")]
+        public Button button;
+        public Text buttonText;
+        public Text outputText;
+        public Text timeText;
+        public Dropdown languageDropdown;
+        public Toggle translateToggle;
+        public Toggle vadToggle;
+        public ScrollRect scroll;
+
+        [Header("Ejercicio 2")]
+        public GameObject warrior;
+        public GameObject sword;
+
+        private string _buffer;
+
+        private void Awake()
+        {
+            whisper.OnNewSegment += OnNewSegment;
+            whisper.OnProgress += OnProgressHandler;
+
+            microphoneRecord.OnRecordStop += OnRecordStop;
+
+            button.onClick.AddListener(OnButtonPressed);
+            languageDropdown.value = languageDropdown.options
+                .FindIndex(op => op.text == whisper.language);
+            languageDropdown.onValueChanged.AddListener(OnLanguageChanged);
+
+            translateToggle.isOn = whisper.translateToEnglish;
+            translateToggle.onValueChanged.AddListener(OnTranslateChanged);
+
+            vadToggle.isOn = microphoneRecord.vadStop;
+            vadToggle.onValueChanged.AddListener(OnVadChanged);
+        }
+
+        private void OnVadChanged(bool vadStop)
+        {
+            microphoneRecord.vadStop = vadStop;
+        }
+
+        private void OnButtonPressed()
+        {
+            if (!microphoneRecord.IsRecording)
+            {
+                microphoneRecord.StartRecord();
+                buttonText.text = "Stop";
+            }
+            else
+            {
+                microphoneRecord.StopRecord();
+                buttonText.text = "Record";
+            }
+        }
+
+        private async void OnRecordStop(AudioChunk recordedAudio)
+        {
+            buttonText.text = "Record";
+            _buffer = "";
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var res = await whisper.GetTextAsync(recordedAudio.Data, recordedAudio.Frequency, recordedAudio.Channels);
+            if (res == null || !outputText)
+                return;
+
+            var time = sw.ElapsedMilliseconds;
+            var rate = recordedAudio.Length / (time * 0.001f);
+            timeText.text = $"Time: {time} ms\nRate: {rate:F1}x";
+
+            var text = res.Result.ToLower(); // lo pasamos a minusculas para el regex
+            if (printLanguage)
+                text += $"\n\nLanguage: {res.Language}";
+
+            outputText.text = text;
+            UiUtils.ScrollDown(scroll);
+
+            DetectRegexText(text); // nueva función 
+        }
+
+        private void OnLanguageChanged(int ind)
+        {
+            var opt = languageDropdown.options[ind];
+            whisper.language = opt.text;
+        }
+
+        private void OnTranslateChanged(bool translate)
+        {
+            whisper.translateToEnglish = translate;
+        }
+
+        private void OnProgressHandler(int progress)
+        {
+            if (!timeText)
+                return;
+            timeText.text = $"Progress: {progress}%";
+        }
+
+        private void OnNewSegment(WhisperSegment segment)
+        {
+            if (!streamSegments || !outputText)
+                return;
+
+            _buffer += segment.Text;
+            outputText.text = _buffer + "...";
+            UiUtils.ScrollDown(scroll);
+        }
+
+        private void DetectRegexText(string text)
+        {
+            // Cambiar la espada a verde
+            if (Regex.IsMatch(text, @"\bverde\b"))
+            {
+                foreach (Renderer r in sword.GetComponentsInChildren<Renderer>())
+                {
+                    r.material.color = Color.green;
+                }
+            }
+
+            // El guerrero se hace grande
+            if (Regex.IsMatch(text, @"\bgrande\b"))
+            {
+                warrior.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+            }
+        }
+    }
+}
